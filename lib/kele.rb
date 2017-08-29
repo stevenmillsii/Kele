5c2c8eeeab5c6ca09c25719960402fb80c83d38a
stevenmills <samills0608@gmail.com>
2017-08-29 18:24:06 -0400

    kele-7: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -37,4 +37,16 @@ class Kele
       },
       headers: { "authorization" => @auth_token })
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post('/checkpoint_submissions',
+      body: {
+        "checkpoint_id": checkpoint_id,
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+      },
+      headers: { "authorization" => @auth_token })
+  end
 end
